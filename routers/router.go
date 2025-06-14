aa7c1a8a9e6b06cf782970dfbff4696e90a34021
Christian J H <142556201+Christiajh@users.noreply.github.com>
2025-06-14 11:12:07 +0700

    Github

--- routers/router.go
+++ routers/router.go
@@ -3,11 +3,20 @@ package routers
 import (
 	"bioskop-api/controllers"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
+	// Endpoint default root "/" agar tidak 404 saat akses domain langsung
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "✅ Bioskop API aktif. Gunakan endpoint /bioskop untuk mengambil data.",
+		})
+	})
+
+	// Endpoint API bioskop
 	r.POST("/bioskop", controllers.CreateBioskop)
 	r.GET("/bioskop", controllers.GetAllBioskop)
 	r.GET("/bioskop/:id", controllers.GetBioskopByID)
